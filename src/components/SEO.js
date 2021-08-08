d529a51b6c4fd4a574917a2da3b0db193e77dfcc
a1p4ca <castlemin.park@gmail.com>
2021-08-08 16:03:00 +0900

    title 수정

--- src/components/SEO.js
+++ src/components/SEO.js
@@ -26,7 +26,7 @@ function Seo({ description, lang, meta, title }) {
       htmlAttributes={{
         lang,
       }}
-      title={title}
+      title={title || defaultTitle}
       titleTemplate={defaultTitle ? `%s | ${defaultTitle}` : null}
       meta={[
         {
